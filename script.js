5c4917ed6c178016b83f1cf59dce0df6b7383078
anandupender <anand.upender@gmail.com>
2021-05-28 08:55:55 -0700

    new mapping of mouth to notes, more like a trumpet

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const video = document.getElementById('video');
 var localStream;
-// var sampler = new Tone.Sampler({
-// 	urls: {
-// 		"G4": "357568_6552981-lq.mp3"
-// 	},
-// 	release: 1,
-// 	baseUrl: "https://freesound.org/data/previews/357/",
-// }).toDestination();
 
 var synth = new Tone.Synth().toMaster();
 
@@ -35,21 +28,33 @@ document.querySelector('.start-button')?.addEventListener('click', async () => {
     }
 })
 
+var mouthMin = .33;
+var mouthMax = .39;
+const notes = {"Bb3":0,"B3":0,"C4":0,"C#4":0,"D4":0,"Eb4":0,"E4":0,"F4":0,"F#4":0,"G4":0,"Ab4":0,"A4":0,"Bb4":0,"B4":0,"C5":0,"C#5":0,"D5":0,"Eb5":0,"E5":0,"F5":0,"F#5":0,"G5":0};
+const numNotes = 22;
+
 const fingerPositions = {
 "0":["C4", "G4", "C5", "E5", "G5"],
 "1":["Bb3","F4","Bb4","D5", "F5"],
-"2":["B3", "F#4", "B4","D#5","F#5"],
+"2":["B3", "F#4", "B4","Eb5","F#5"],
 "3":[],
 "4":["D4", "G4"],
 "5":["Eb4","Ab4","Eb5"],
 "6":["E4","A4","C#5","E5"],
 "7":["C#4"]};
 
+// setup notes
+var counter = 0;
+for (var note in notes) {
+    notes[note] = scale(counter, 0, numNotes, mouthMin, mouthMax);
+    counter++;
+}
+
+console.log(notes);
+
 var key1 = document.querySelector(".key#first");
 var key2 = document.querySelector(".key#second");
 var key3 = document.querySelector(".key#third");
-
-const testOpen = ["C4", "G4", "C5", "E5", "G5"];
 
 var currPositionArray = fingerPositions["0"];
 
@@ -114,9 +119,9 @@ function changeFingering(){
     }
 
     // change note visually and play
-    if(currPositionArray[note] != undefined){
-        document.querySelector("#note").innerHTML = currPositionArray[note];
-        synth.setNote(currPositionArray[note]);
+    if(note != undefined){
+        document.querySelector("#note").innerHTML = note;
+        synth.setNote(note);
     }
 
 }
@@ -158,6 +163,10 @@ function scale (number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
+function difference(a, b) {
+    return Math.abs(a - b);
+}
+
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.querySelector(".video").append(canvas)
@@ -184,12 +193,24 @@ video.addEventListener('play', () => {
         // ToDo: Calibrate based on face
 
         // map mouth width to notes in array
-        note = Math.round(scale(mouthSizeRelativeToFace, .32, .39, 0, currPositionArray.length - 1));
+        // note = Math.round(scale(mouthSizeRelativeToFace, mouthMin, mouthMax, 0, currPositionArray.length - 1));
+
+        // find closest note with current fingering
+        var closestNote;
+        var lowestDistance = 100;
+        for(var i = 0; i < currPositionArray.length;i++){
+            var diff = difference(mouthSizeRelativeToFace,notes[currPositionArray[i]]);
+            if(diff < lowestDistance){
+                lowestDistance = diff;
+                closestNote = currPositionArray[i];
+            }
+        }
+        note = closestNote;
 
         // change note visually and play
-        if(note !== prevNote && currPositionArray[note] != undefined){
-            document.querySelector("#note").innerHTML = currPositionArray[note];
-            synth.setNote(currPositionArray[note]);
+        if(note !== prevNote && note != undefined){
+            document.querySelector("#note").innerHTML = note;
+            synth.setNote(note);
         }
         prevNote = note;
 
