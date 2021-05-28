70cb27c20e633d0fc315f1a2b7aab757e5db37db
anandupender <anand.upender@gmail.com>
2021-05-28 09:42:40 -0700

    value smoothening and embouchure progress bar

--- script.js
+++ script.js
@@ -30,18 +30,18 @@ document.querySelector('.start-button')?.addEventListener('click', async () => {
 
 var mouthMin = .33;
 var mouthMax = .39;
-const notes = {"Bb3":0,"B3":0,"C4":0,"C#4":0,"D4":0,"Eb4":0,"E4":0,"F4":0,"F#4":0,"G4":0,"Ab4":0,"A4":0,"Bb4":0,"B4":0,"C5":0,"C#5":0,"D5":0,"Eb5":0,"E5":0,"F5":0,"F#5":0,"G5":0};
-const numNotes = 22;
+const notes = {"F#3":0,"G3":0,"Ab3":0,"A3":0,"Bb3":0,"B3":0,"C4":0,"C#4":0,"D4":0,"Eb4":0,"E4":0,"F4":0,"F#4":0,"G4":0,"Ab4":0,"A4":0,"Bb4":0,"B4":0,"C5":0,"C#5":0,"D5":0,"Eb5":0,"E5":0,"F5":0,"F#5":0,"G5":0};
+const numNotes = 26;
 
 const fingerPositions = {
 "0":["C4", "G4", "C5", "E5", "G5"],
 "1":["Bb3","F4","Bb4","D5", "F5"],
 "2":["B3", "F#4", "B4","Eb5","F#5"],
 "3":[],
-"4":["D4", "G4"],
-"5":["Eb4","Ab4","Eb5"],
-"6":["E4","A4","C#5","E5"],
-"7":["C#4"]};
+"4":["G3","D4", "G4", "B4","G5"],
+"5":["Ab3","Eb4","Ab4","Eb5"],
+"6":["A3","E4","A4","C#5","E5"],
+"7":["F#3","C#4","F#4", "B4", "D5", "G5"]};
 
 // setup notes
 var counter = 0;
@@ -63,6 +63,7 @@ var currKeys = {"1": false, "2": false, "3": false};
 //last face read value
 var note = 0;
 var prevNote = 0;
+var runningAverageArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 // figure out key mappings
 document.addEventListener('keydown', keyPress);
@@ -167,9 +168,13 @@ function difference(a, b) {
     return Math.abs(a - b);
 }
 
+function average(nums) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+}
+
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
-  document.querySelector(".video").append(canvas)
+  document.querySelector("#videoContainer").append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   interval = setInterval(async () => {
@@ -190,22 +195,30 @@ video.addEventListener('play', () => {
         
         const mouthSizeRelativeToFace = mouthWidth / faceWidth;// values vary from .33 to .38 based on lips
 
-        // ToDo: Calibrate based on face
-
-        // map mouth width to notes in array
-        // note = Math.round(scale(mouthSizeRelativeToFace, mouthMin, mouthMax, 0, currPositionArray.length - 1));
+        // Problem: values from the facial recognizer are too jittery. Take running average to smooth
+        // Solution: running average to smooth results
+        runningAverageArray.push(mouthSizeRelativeToFace);
+        runningAverageArray.shift();
+        var currAverage = average(runningAverageArray);
 
         // find closest note with current fingering
         var closestNote;
         var lowestDistance = 100;
         for(var i = 0; i < currPositionArray.length;i++){
-            var diff = difference(mouthSizeRelativeToFace,notes[currPositionArray[i]]);
+            var diff = difference(currAverage,notes[currPositionArray[i]]);
             if(diff < lowestDistance){
                 lowestDistance = diff;
                 closestNote = currPositionArray[i];
             }
         }
         note = closestNote;
+
+        var percentageEmbouchure = (currAverage - mouthMin)/(mouthMax - mouthMin)*100;
+        if(percentageEmbouchure < 0){
+            percentageEmbouchure = 0;
+        }
+        document.querySelector("#sliderInside").style.height = percentageEmbouchure + "%";
+
 
         // change note visually and play
         if(note !== prevNote && note != undefined){
@@ -219,5 +232,5 @@ video.addEventListener('play', () => {
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     }
 
-  }, 70)
+  }, 30)
 })
